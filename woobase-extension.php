56512d3651e03771df05c39dc83d8b61a8addc04
wpuipress <120018841+wpuipress@users.noreply.github.com>
2024-07-13 19:25:34 +0100

    First commit

---
+++ woobase-extension.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: WooBase Plugin example
+Plugin URI: https://woobase.com
+Description: This is a demo plugin to show how to hook into to woobase and add custom fields and modify the interface
+Version: 1.0.0
+Author: woobase
+*/
+
+// Prevent direct access to this file
+defined("ABSPATH") || exit();
